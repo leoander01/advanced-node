f0f0b3bf37af8bb304c349a151553a85831f0ef5
Leoander Neves <leoanderneves8@outlook.com>
2024-01-25 20:36:02 -0300

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb+srv://leo:mo5zqLAvvfbuOYrG@cluster-advanced-node.jzm7rjn.mongodb.net/",
+  cookieKey: "123123123",
+};
