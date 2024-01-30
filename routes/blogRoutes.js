f2b8a063377a36ac6034e8a4a549043dcd3e8e6b
Leoander Neves <leoanderneves8@outlook.com>
2024-01-30 20:36:39 -0300

    add cache file

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,22 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis');
+    const util = require('util');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    client.get = util.promisify(client.get);
+    const cachedBlogs = await client.get(req.user.id);
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
 
+    const blogs = await Blog.find({ _user: req.user.id });
+    
+    console.log('SERVING FROM MONGODB');
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
