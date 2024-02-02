0d70599287dbf7ab7507622ee6431b02418a2518
Leoander Neves <leoanderneves8@outlook.com>
2024-02-02 20:53:46 -0300

    add jest and puppeteer

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://leo:mo5zqLAvvfbuOYrG@cluster-advanced-node.jzm7rjn.mongodb.net/",
+    "mongodb+srv://leo:rTCs9akceqaZ3rb2@cluster0.isdgspw.mongodb.net/?retryWrites=true&w=majority",
   cookieKey: "123123123",
 };
