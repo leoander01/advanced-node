d8abc17768f089d9cbe48e4672f856fab8dcd1e7
Leoander Neves <leoanderneves8@outlook.com>
2024-02-27 20:10:29 -0300

    asserting form display

--- tests/header.test.js
+++ tests/header.test.js
@@ -12,7 +12,8 @@ afterEach(async () => {
 });
 
 test('the header has the correct text', async () => {
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  // const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  const text = await page.getContentsOf('a.brand-logo');
   expect(text).toEqual('Blogster');
 });
 
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -28,8 +28,12 @@ class CustomPage {
     const { session, sig } = sessionFactory(user);
     await this.page.setCookie({ name: 'session', value: session });
     await this.page.setCookie({ name: 'session.sig', value: sig });
-    await this.page.goto('localhost:3000');
+    await this.page.goto('localhost:3000/blogs');
     await this.page.waitFor('a[href="/auth/logout"]');
+  }
+
+  async getContentsOf(selector) {
+    return this.page.$eval(selector, el => el.innerHTML);
   }
 }
 
