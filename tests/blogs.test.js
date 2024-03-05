ee07bf21f32165ec7a66725f55df6e44b4a186fb
Leoander Neves <leoanderneves8@outlook.com>
2024-03-05 17:34:56 -0300

    asserting blog creation

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -22,12 +22,34 @@ describe('when logged in', async () => {
     expect(label).toEqual('Blog Title');
   });
 
+  describe('and using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button');
+    });
+    
+    test('submitting takes user to review screen', async () => {
+      const text = await page.getContentsOf('h5');
+      expect(text).toEqual('Please confirm your entries');
+    });
+    
+    // test('submitting the saving adds blog to index page', async () => {
+    //   await page.click('button.green');
+    //   await page.waitFor('.card');
+    //   const title = await page.getContentsOf('.card-title');
+    //   const content = await page.getContentsOf('p');
+    //   expect(title).toEqual('My Title');
+    //   expect(content).toEqual('My Content');
+    // });
+  });
+
   describe('and using invalid inputs', async () => {
     beforeEach(async () => {
       await page.click('form button');
     });
  
-    test('the form sows an error message', async () => {
+    test('the form shows an error message', async () => {
       const titleError = await page.getContentsOf('.title .red-text');
       const contentError = await page.getContentsOf('.content .red-text');
       expect(titleError).toEqual('You must provide a value');
