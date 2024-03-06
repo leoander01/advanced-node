c392a8f79eabe4592c4aae97debad94b78e39c94
Leoander Neves <leoanderneves8@outlook.com>
2024-03-06 17:12:00 -0300

    advanced test helpers

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const cleanCash = require('../middlewares/cleanCache');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -21,7 +21,7 @@ module.exports = app => {
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, cleanCash, async (req, res) => {
+  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
@@ -36,7 +36,5 @@ module.exports = app => {
     } catch (err) {
       res.send(400, err);
     }
-
-    clearHash(req.user.id);
   });
 };
--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -57,3 +57,37 @@ describe('when logged in', async () => {
     });
   });
 });
+
+describe('user is not logged in', async () => {
+  const actions = [
+    {
+      method: 'get',
+      path: '/api/blogs'
+    },
+    {
+      method: 'post',
+      path: '/api/blogs',
+      data: {
+        title: 'T',
+        content: 'C',
+      }
+    }
+  ];
+
+  test('blog related actions are prohibited', async () => {
+    const results = await page.execRequests(actions);
+    for (let result of results) {
+      expect(result).toEqual({ error: 'You must log in!' });
+    }
+  });
+
+  // test('user cannot create blog posts', async () => {
+  //   const result = await page.post('/api/blogs', { title: 'T', content: 'C' });
+  //   expect(result).toEqual({ error: 'You must log in!' });
+  // });
+
+  // test('user cannot get a list of posts', async () => {
+  //   const result = await page.get('/api/blogs');
+  //   expect(result).toEqual({ error: 'You must log in!' });
+  // });
+});
